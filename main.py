03e9b4dd331a3c2f81e15fe010f34c5aa5509799
jud-ie19 <owuorakinyijudith@gmail.com>
2022-04-14 16:45:08 +0300

    firstcommit

---
+++ main.py
@@ -0,0 +1,13 @@
+import calendar
+import time
+
+print(calendar.weekheader(3))
+print()
+
+print(calendar.firstweekday())
+print()
+
+print(calendar.month(2022,4))
+print(calendar.monthcalendar(2022,4))
+print(calendar.calendar(2022))
+
